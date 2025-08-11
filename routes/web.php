430eb4cd913b4f3614403663b61d0dc5993bd02f
Fakrul Hasan Sajib <ahmedsajib1999s@gmail.com>
2025-08-11 16:52:07 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Vendor\VendorDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::middleware(['auth', 'role:vendor'])->prefix('vendor')->name('vendor.')->group(function () {
+    Route::get('/dashboard', [VendorDashboardController::class, 'index'])->name('dashboard');
+    // এখানে ভেন্ডরের অন্যান্য রাউট (যেমন: product management) যোগ হবে
 });
